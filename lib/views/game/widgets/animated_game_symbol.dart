487b3e7d7c602c022b8d25330bbacb926257f463
RekanAnwar <rekan545@gmail.com>
2025-01-15 18:15:39 +0300

    Refactor AnimatedGameSymbol widget for improved performance and readability
    - Removed unnecessary TweenAnimationBuilder components to simplify the animation logic.
    - Replaced animations with direct Transform.rotate calls for better performance.
    - Streamlined the widget structure and reduced code duplication in the AnimatedGameSymbol class.
    
    These changes enhance the efficiency and maintainability of the animated game symbol rendering.

--- lib/views/game/widgets/animated_game_symbol.dart
+++ lib/views/game/widgets/animated_game_symbol.dart
@@ -35,61 +35,43 @@ class AnimatedGameSymbol extends StatelessWidget {
       child: Stack(
         alignment: Alignment.center,
         children: [
-          TweenAnimationBuilder(
-            duration: const Duration(milliseconds: 300),
-            tween: Tween<double>(begin: -1.0, end: 0.0),
-            builder: (context, value, child) {
-              return Transform.translate(
-                offset: Offset(0, value * size),
-                child: Transform.rotate(
-                  angle: 45 * 3.14159 / 180,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        colors: [Colors.red, Colors.redAccent],
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 4,
-                          offset: Offset(0, 2),
-                        ),
-                      ],
-                    ),
-                    height: 8,
-                  ),
+          Transform.rotate(
+            angle: 45 * 3.14159 / 180,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [Colors.red, Colors.redAccent],
                 ),
-              );
-            },
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              height: 8,
+            ),
           ),
-          TweenAnimationBuilder(
-            duration: const Duration(milliseconds: 300),
-            tween: Tween<double>(begin: -1.0, end: 0.0),
-            builder: (context, value, child) {
-              return Transform.translate(
-                offset: Offset(0, value * size),
-                child: Transform.rotate(
-                  angle: -45 * 3.14159 / 180,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        colors: [Colors.red, Colors.redAccent],
-                      ),
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 4,
-                          offset: Offset(0, 2),
-                        ),
-                      ],
-                    ),
-                    height: 8,
-                  ),
+          Transform.rotate(
+            angle: -45 * 3.14159 / 180,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [Colors.red, Colors.redAccent],
                 ),
-              );
-            },
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              height: 8,
+            ),
           ),
         ],
       ),
@@ -97,28 +79,19 @@ class AnimatedGameSymbol extends StatelessWidget {
   }
 
   Widget _buildO() {
-    return TweenAnimationBuilder(
-      duration: const Duration(milliseconds: 300),
-      tween: Tween<double>(begin: -1.0, end: 0.0),
-      builder: (context, value, child) {
-        return Transform.translate(
-          offset: Offset(0, value * size),
-          child: Container(
-            width: size,
-            height: size,
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [Colors.blue, Colors.blueAccent],
-              ),
-              borderRadius: BorderRadius.circular(size / 2),
-              border: Border.all(
-                color: Colors.transparent,
-                width: 8,
-              ),
-            ),
-          ),
-        );
-      },
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [Colors.blue, Colors.blueAccent],
+        ),
+        borderRadius: BorderRadius.circular(size / 2),
+        border: Border.all(
+          color: Colors.transparent,
+          width: 8,
+        ),
+      ),
     );
   }
 }
